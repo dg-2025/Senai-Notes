7b2fcf8218ed0cf69f0c493a635d59ebadbf81c6
dg-2025 <danielgomes5039@gmail.com>
2025-09-25 19:57:22 -0300

    tabelas

--- Banco de Dados/Tabelas.sql
+++ Banco de Dados/Tabelas.sql
@@ -1,23 +1,41 @@
+--Criar schema
 create schema notes
-create table notes.usuario (
 
+--Criar Tabela de usuario
+create table notes.usuario (
 	id_usuario INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	data_criacao TIMESTAMPTZ,
 	nome TEXT NOT NULL,
 	email TEXT NOT NULL,
-	senha VARCHAR (12) NOT NULL
+	senha VARCHAR (12) NOT NULL,
+	data_criacao TIMESTAMPTZ
 	
 );
+
+--Criar Tabela de Tags
+create table notes.tags(
+	id_tag int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_usuario int,
+	FOREIGN KEY (id_usuario) REFERENCES notes.usuario(id_usuario),
+	nome text not null,
+	descricao text,
+	data_criacao TIMESTAMPTZ
+);
+
+--Criar Tabela de Nota
 create table notes.nota (
 
 	id_nota INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	id_usuario INT,
 	FOREIGN KEY (id_usuario) REFERENCES notes.usuario(id_usuario),
-	data_criacao TIMESTAMPTZ,
-	ultima_edicao TIMESTAMPTZ,
 	titulo TEXT NOT NULL,
-	tag TEXT,
+	id_tag int,
+	FOREIGN KEY (id_tag) REFERENCES notes.tags(id_tag),
 	descricao TEXT,
 	imagem TEXT,
-	arquivado BOOLEAN
+	arquivado BOOLEAN,
+	data_criacao TIMESTAMPTZ,
+	ultima_edicao TIMESTAMPTZ
 );
+
+
+
